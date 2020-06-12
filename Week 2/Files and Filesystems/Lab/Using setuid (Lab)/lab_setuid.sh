128706dd408d46ddbf3ca7ab1e19edee279c827c
sambhipiyush <piyush.sambhi07@icloud.com>
2020-06-13 05:00:18 +0530

    First Commit

---
+++ Week 2/Files and Filesystems/Lab/Using setuid (Lab)/lab_setuid.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#/* **************** Coursera **************** */
+#/*
+# * The code herein is: Copyright the Linux Foundation, 2018
+# *
+# * This Copyright is retained for the purpose of protecting free
+# * redistribution of source.
+# *
+# *     URL:    http://training.linuxfoundation.org
+# *     email:  trainingquestions@linuxfoundation.org
+# *
+# * This code is distributed under Version 2 of the GNU General Public
+# * License, which you should have received with the source.
+# *
+# */
+#!/bin/bash
+
+set -x
+
+gcc -o lab3_write -O2 lab3_write.c
+
+echo "PASSWORD_HERE" | sudo -S chown root.root lab3_write
+#sudo chown root.root lab3_write
+
+echo "PASSWORD_HERE" | sudo -S chmod +s lab3_write
+#sudo chmod +s lab3_write
+
